8ef0fde53d1c188104bc789ce0a4ce79649148cd
weidi_power <1195660830@qq.com>
2018-11-12 23:16:00 +0800

    init project

---
+++ app.js
@@ -0,0 +1,16 @@
+// app.js 
+const Hapi = require('hapi'); 
+const server = new Hapi.Server(); // 配置服务器启动 host 与端口 
+server.connection({ port: 3000 }); 
+const init = async () => { 
+	server.route([ // 创建一个简单的 hello hapi 接口 
+		{ method: 'GET', path: '/', handler: (request, reply) => { 
+			reply('hello hapi power by require hapi!!'); 
+		}, 
+	}, 
+]); // 启动服务 
+await server.start(); 
+console.log(`Server running at: ${server.info.url};`);
+ }; 
+ init();
+
