cace18019544bc0bd1b78f6adbadeb8606adb657
1195660830 <1195660830@qq.com>
2018-12-04 21:41:40 +0800

    'update'

--- routes/hello-hapi.js
+++ routes/hello-hapi.js
@@ -16,7 +16,7 @@ method: 'GET',
 			config: {
 				tags: ['api','test'],
 				auth:false,
-				description: '测试hello-hapi',
+				description: '已使用 jwt,需要首先使用签发机构的接口获取 token,才能使用,但是在 上一行 auth 设置为 false , 即可随意进入',
 				validate: {
 					...jwtHeaderDefine, // 增加需要 jwt auth 认证的接口 header 校验
 				  },
