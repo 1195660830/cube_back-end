ffc6bd985f0f973d99735c44a229a73617c2ffbe
wade <1195660830@qq.com>
2019-03-29 23:08:03 +0800

    获取微信用户信息

--- routes/tests.js
+++ routes/tests.js
@@ -3,7 +3,7 @@
  * @fileoverview 测试swagger
  * @author Wade
  */
-
+const models = require('../models');
 const { jwtHeaderDefine } = require('../utils/router-helper');
 
 var crypto = require('crypto');
@@ -47,9 +47,34 @@ module.exports = [
     method: 'POST',
     path: '/weapp/getUserInfo',
     handler: (request, reply) => {
-     console.log(request.auth.credentials); // 控制台输出 { userId: 1}
-      reply('秘钥有效'+request.auth.credentials.userId);
-      // reply(request.auth.credentials);
+     console.log('查询',request.auth.credentials.userId); // 控制台输出 { userId: 1}
+
+     models.sequelize.transaction(function (t) {
+      return models.users
+        .findAll({
+          where: {
+            id: request.auth.credentials.userId
+          },
+          transaction: t
+        })
+        .spread((user, created) => {
+          // console.log(
+          //   user.get({
+          //     plain: true
+          //   })
+          // );
+
+         
+          reply({
+            message: '秘钥有效',
+            userID: request.auth.credentials.userId,
+            userInfo: user,
+          });
+
+          
+        })
+    })
+
     },
     config: {
       tags: ['api', 'tests'],
