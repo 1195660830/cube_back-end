f6b7b7478cc8518672db07d70e43a171011f5194
wade <1195660830@qq.com>
2019-05-17 14:51:29 +0800

    添加注释

--- models/index.js
+++ models/index.js
@@ -6,35 +6,35 @@ const configs = require('../config/config.js');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = {
-  ...configs[env],
-  define: {
-    underscored: true, //尝试 去除,与 设 false ,都会报 找不到 createdAt, true 适配 数据库 created_at
-  },
+    ...configs[env],
+    define: {
+        underscored: true, //尝试 去除,与 设 false ,都会报 找不到 createdAt, true 适配 数据库 created_at
+    },
 };
 const db = {};
 let sequelize = null;
 
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
-  .readdirSync(__dirname)
-  .filter((file) => {
-    const result = file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
-    return result;
-  })
-  .forEach((file) => {
-    const model = sequelize.import(path.join(__dirname, file));
-    db[model.name] = model;
-  });
+    .readdirSync(__dirname) //__dirname node.js 当前模块目录名
+    .filter((file) => {
+        const result = file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
+        return result;
+    })
+    .forEach((file) => {
+        const model = sequelize.import(path.join(__dirname, file));
+        db[model.name] = model;
+    });
 
 Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
+    if (db[modelName].associate) {
         db[modelName].associate(db);
-      }
+    }
 });
 
 db.sequelize = sequelize;
