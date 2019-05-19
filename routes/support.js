8452b6e71bdc8294cc3d04137764e574907c2a51
wade <1195660830@qq.com>
2019-05-19 08:12:58 +0800

    视频文件上传功能，并完善swagger

--- routes/support.js
+++ routes/support.js
@@ -2,7 +2,7 @@
  * @Author: wade 
  * @Date: 2019-05-17 14:57:25 
  * @Last Modified by: wade
- * @Last Modified time: 2019-05-19 07:57:11
+ * @Last Modified time: 2019-05-19 08:09:43
  * 
  * 开发文件上传功能
  */
@@ -59,6 +59,63 @@ module.exports = [{
             auth: false,
             description: '访问publice文件夹下的图片资源',
             tags: ['api', `${GROUP_NAME}`],
+            validate: {
+                params: {
+                    imageTarget: Joi.string().description('你将要查询的图片文件名称')
+                }
+            },
+        }
+    }, {
+        method: 'POST',
+        path: '/upload/video',
+        handler: async(request, reply) => {
+            let filename = Math.random().toString(36).substr(2) + '.' + request.payload.file.hapi.filename.split('.').pop()
+            console.log(filename)
+            fs.writeFile(__dirname + `/publice/video/${filename}`, request.payload.file._data, 'utf8', function(error) {
+                if (error) {
+                    console.log(error);
+                    return false;
+                }
+                reply(`${filename}`);
+            })
+        },
+        config: {
+            auth: false,
+            plugins: {
+                'hapi-swagger': {
+                    payloadType: 'form'
+                }
+            },
+            description: '上传视频文件资源',
+            tags: ['api', `${GROUP_NAME}`],
+            validate: {
+                payload: {
+                    file: Joi.any().meta({ swaggerType: 'file' }).description('你将要上传的视频文件')
+                }
+            },
+            payload: {
+                maxBytes: 3145728,
+                parse: true,
+                output: 'stream'
+            }
+        }
+    },
+    {
+        method: 'GET',
+        path: '/video/{videoTarget}',
+        handler: function(request, reply) {
+            const { videoTarget: videoName } = request.params;
+            reply.file(__dirname + `/publice/video/${videoName}`);
+        },
+        config: {
+            auth: false,
+            description: '访问publice文件夹下的视频文件资源',
+            tags: ['api', `${GROUP_NAME}`],
+            validate: {
+                params: {
+                    videoTarget: Joi.string().description('你将要查询的视频文件名称')
+                }
+            },
         }
     },
 ]
